c0714f3ea54eb5d5cc0fdab603f02858ff592891
davidwpowell <david.w.powell90@gmail.com>
2016-07-07 20:12:42 -0400

    Added random Shakespearean insult generator

--- routes.py
+++ routes.py
@@ -1,12 +1,18 @@
 import os
 from flask import Flask
+import insulter
 
 app = Flask(__name__)
 # Keeps Flask from swallowing error messages
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
 @app.route("/")
 def insult():
-    return "Hello, Ryan! You Butt-Muncher!"
+    return insulter.insult()
+
+@app.route("/<name>")
+def insult_name():
+    return insulter.named_insult(name)
 
 if __name__ == "__main__":
     app.run()
